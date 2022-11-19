c4a07d81085c82734d6df5e7a676163ad8a93e3e
Shubham Krishna <shubham.krishna@ml6.eu>
2022-11-19 17:44:25 +0530

    Add Solution

---
+++ Problems/Fast_Slow Pointers/Linked List Cycle/solution.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next)
+            return false;
+
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+        while(slow && fast){
+            slow = slow->next;
+            fast = fast->next?fast->next->next:NULL;
+            if(slow == fast)
+                return true;
+        }
+        return false;
+    }
+};
---
+++ Problems/Fast_Slow Pointers/Linked List Cycle/solution.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        if head is None or head.next is None:
+            return False
+        while slow is not None and fast is not None:
+            slow = slow.next
+            fast = fast.next.next if fast.next is not None else None
+            if slow == fast:
+                return True
+
+        return False
+
